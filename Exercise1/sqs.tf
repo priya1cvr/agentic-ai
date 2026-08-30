c34fd6f482e50d901a5ef16df0ae964b61049ba4
priya1cvr <priya1cvr.pb@gmail.com>
2026-08-30 15:27:34 +0530

    Delete Exercise1 directory

--- Exercise1/sqs.tf
+++
@@ -1,7 +0,0 @@
-# resource = Terraform is creating real infrastructure
-# aws_sqs_queue = the AWS SQS resource type
-# "poc_queue" = the name you gave this resource inside Terraform
-
-resource "aws_sqs_queue" "poc_queue" {
-  name = "POC-Queue"
-}
